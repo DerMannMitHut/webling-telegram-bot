93d0baaa7f1b5734d871ac71f9219532d14ee1f7
DerMannMitHut <dom@waesch-online.de>
2025-08-03 23:50:41 +0200

    add example config file

---
+++ webhooks/wh_config/config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'TELEGRAM_BOT_TOKEN' => '1234:abcd',
+    'WEBLING_API_KEY'    => '1234abcd',
+    'WEBLING_BASE_URL'   => 'https://verein.webling.ch',
+    'WEBLING_MEMBER_GROUP_OPEN' => '100',
+    'WEBLING_MEMBER_GROUP_ACCEPTED' => '200',
+    'WEBLING_MEMBER_GROUP_DECLINED' => '300',
+    'SMTP_HOST' => 'smtp.strato.de',
+    'SMTP_PORT' => '587',
+    'SMTP_USER' => 'username@example.com',
+    'SMTP_PASS' => 'abcd1234',
+    'SMTP_FROM' => 'username@example.com',
+];
+
